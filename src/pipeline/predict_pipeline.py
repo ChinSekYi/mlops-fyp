c48f2e7f12f33e1969dceb8e82ade0e90843a234
Chin Sek Yi <sekyichin@gmail.com>
2025-08-20 00:49:51 +0800

    Remove .DS_Store files from repository and add to .gitignore

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
 
@@ -14,207 +14,105 @@ class PredictPipeline:
             pred_result = model.predict(data_scaled)
             return pred_result
         except Exception as e:
-            raise CustomException(e, sys)
-        )
-        self.operating_expenses_short_term_liabilities = (
-            operating_expenses_short_term_liabilities
-        )
-        self.operating_expenses_total_liabilities = operating_expenses_total_liabilities
-        self.profit_on_sales_total_assets = profit_on_sales_total_assets
-        self.total_sales_total_assets = total_sales_total_assets
-        self.current_assets_inventories_long_term_liabilities = (
-            current_assets_inventories_long_term_liabilities
-        )
-        self.constant_capital_total_assets = constant_capital_total_assets
-        self.profit_on_sales_sales = profit_on_sales_sales
-        self.current_assets_inventory_receivables_short_term_liabilities = (
-            current_assets_inventory_receivables_short_term_liabilities
-        )
-        self.total_liabilities_profit_on_operating_activities_depreciation_12_365 = (
-            total_liabilities_profit_on_operating_activities_depreciation_12_365
-        )
-        self.profit_on_operating_activities_sales = profit_on_operating_activities_sales
-        self.rotation_receivables_inventory_turnover_days = (
-            rotation_receivables_inventory_turnover_days
-        )
-        self.receivables_365_sales = receivables_365_sales
-        self.net_profit_inventory = net_profit_inventory
-        self.current_assets_inventory_short_term_liabilities = (
-            current_assets_inventory_short_term_liabilities
-        )
-        self.inventory_365_cost_of_products_sold = inventory_365_cost_of_products_sold
-        self.EBITDA_profit_on_operating_activities_depreciation_total_assets = (
-            EBITDA_profit_on_operating_activities_depreciation_total_assets
-        )
-        self.EBITDA_profit_on_operating_activities_depreciation_sales = (
-            EBITDA_profit_on_operating_activities_depreciation_sales
-        )
-        self.current_assets_total_liabilities = current_assets_total_liabilities
-        self.short_term_liabilities_total_assets = short_term_liabilities_total_assets
-        self.short_term_liabilities_365_cost_of_products_sold = (
-            short_term_liabilities_365_cost_of_products_sold
-        )
-        self.equity_fixed_assets = equity_fixed_assets
-        self.constant_capital_fixed_assets = constant_capital_fixed_assets
-        self.working_capital = working_capital
-        self.sales_cost_of_products_sold_sales = sales_cost_of_products_sold_sales
-        self.current_assets_inventory_short_term_liabilities_sales_gross_profit_depreciation = current_assets_inventory_short_term_liabilities_sales_gross_profit_depreciation
-        self.total_costs_total_sales = total_costs_total_sales
-        self.long_term_liabilities_equity = long_term_liabilities_equity
-        self.sales_inventory = sales_inventory
-        self.sales_receivables = sales_receivables
-        self.short_term_liabilities_365_sales = short_term_liabilities_365_sales
-        self.sales_short_term_liabilities = sales_short_term_liabilities
-        self.sales_fixed_assets = sales_fixed_assets
+            raise CustomException(e, None)
+
+class CustomData:
+    """
+    responsible for taking the inputs that we are giving in the HTML to the backend
+    """
+
+
+    def __init__(
+        self,
+        Time: float,
+        V1: float, V2: float, V3: float, V4: float, V5: float, V6: float, V7: float, V8: float, V9: float, V10: float,
+        V11: float, V12: float, V13: float, V14: float, V15: float, V16: float, V17: float, V18: float, V19: float, V20: float,
+        V21: float, V22: float, V23: float, V24: float, V25: float, V26: float, V27: float, V28: float,
+        Amount: float
+    ):
+        self.Time = Time
+        self.V1 = V1
+        self.V2 = V2
+        self.V3 = V3
+        self.V4 = V4
+        self.V5 = V5
+        self.V6 = V6
+        self.V7 = V7
+        self.V8 = V8
+        self.V9 = V9
+        self.V10 = V10
+        self.V11 = V11
+        self.V12 = V12
+        self.V13 = V13
+        self.V14 = V14
+        self.V15 = V15
+        self.V16 = V16
+        self.V17 = V17
+        self.V18 = V18
+        self.V19 = V19
+        self.V20 = V20
+        self.V21 = V21
+        self.V22 = V22
+        self.V23 = V23
+        self.V24 = V24
+        self.V25 = V25
+        self.V26 = V26
+        self.V27 = V27
+        self.V28 = V28
+        self.Amount = Amount
+
 
     def get_data_as_dataframe(self):
-        """
-        Converts the attributes of CustomData into a pandas DataFrame.
-
-        Returns:
-            pd.DataFrame: A DataFrame where each row represents a financial metric attribute.
-        """
         try:
             custom_data_input = {
-                "net profit / total assets": [self.net_profit_total_assets],
-                "total liabilities / total assets": [
-                    self.total_liabilities_total_assets
-                ],
-                "working capital / total assets": [self.working_capital_total_assets],
-                "current assets / short-term liabilities": [
-                    self.current_assets_short_term_liabilities
-                ],
-                "[(cash + short-term securities + receivables - short-term liabilities) / (operating expenses - depreciation)] * 365": [
-                    self.cash_short_term_securities_receivables_short_term_liabilities_operating_expenses_depreciation_365
-                ],
-                "retained earnings / total assets": [
-                    self.retained_earnings_total_assets
-                ],
-                "EBIT / total assets": [self.EBIT_total_assets],
-                "book value of equity / total liabilities": [
-                    self.book_value_of_equity_total_liabilities
-                ],
-                "sales / total assets": [self.sales_total_assets],
-                "equity / total assets": [self.equity_total_assets],
-                "(gross profit + extraordinary items + financial expenses) / total assets": [
-                    self.gross_profit_extraordinary_items_financial_expenses_total_assets
-                ],
-                "gross profit / short-term liabilities": [
-                    self.gross_profit_short_term_liabilities
-                ],
-                "(gross profit + depreciation) / sales": [
-                    self.gross_profit_depreciation_sales
-                ],
-                "(gross profit + interest) / total assets": [
-                    self.gross_profit_interest_total_assets
-                ],
-                "(total liabilities * 365) / (gross profit + depreciation)": [
-                    self.total_liabilities_365_gross_profit_depreciation
-                ],
-                "(gross profit + depreciation) / total liabilities": [
-                    self.gross_profit_depreciation_total_liabilities
-                ],
-                "total assets / total liabilities": [
-                    self.total_assets_total_liabilities
-                ],
-                "gross profit / total assets": [self.gross_profit_total_assets],
-                "gross profit / sales": [self.gross_profit_sales],
-                "(inventory * 365) / sales": [self.inventory_365_sales],
-                "sales (n) / sales (n-1)": [self.sales_n_sales_n_1],
-                "profit on operating activities / total assets": [
-                    self.profit_on_operating_activities_total_assets
-                ],
-                "net profit / sales": [self.net_profit_sales],
-                "gross profit (in 3 years) / total assets": [
-                    self.gross_profit_3_years_total_assets
-                ],
-                "(equity - share capital) / total assets": [
-                    self.equity_share_capital_total_assets
-                ],
-                "(net profit + depreciation) / total liabilities": [
-                    self.net_profit_depreciation_total_liabilities
-                ],
-                "profit on operating activities / financial expenses": [
-                    self.profit_on_operating_activities_financial_expenses
-                ],
-                "working capital / fixed assets": [self.working_capital_fixed_assets],
-                "logarithm of total assets": [self.logarithm_total_assets],
-                "(total liabilities - cash) / sales": [
-                    self.total_liabilities_cash_sales
-                ],
-                "(gross profit + interest) / sales": [self.gross_profit_interest_sales],
-                "(current liabilities * 365) / cost of products sold": [
-                    self.current_liabilities_365_cost_of_products_sold
-                ],
-                "operating expenses / short-term liabilities": [
-                    self.operating_expenses_short_term_liabilities
-                ],
-                "operating expenses / total liabilities": [
-                    self.operating_expenses_total_liabilities
-                ],
-                "profit on sales / total assets": [self.profit_on_sales_total_assets],
-                "total sales / total assets": [self.total_sales_total_assets],
-                "(current assets - inventories) / long-term liabilities": [
-                    self.current_assets_inventories_long_term_liabilities
-                ],
-                "constant capital / total assets": [self.constant_capital_total_assets],
-                "profit on sales / sales": [self.profit_on_sales_sales],
-                "(current assets - inventory - receivables) / short-term liabilities": [
-                    self.current_assets_inventory_receivables_short_term_liabilities
-                ],
-                "total liabilities / ((profit on operating activities + depreciation) * (12/365))": [
-                    self.total_liabilities_profit_on_operating_activities_depreciation_12_365
-                ],
-                "profit on operating activities / sales": [
-                    self.profit_on_operating_activities_sales
-                ],
-                "rotation receivables + inventory turnover in days": [
-                    self.rotation_receivables_inventory_turnover_days
-                ],
-                "(receivables * 365) / sales": [self.receivables_365_sales],
-                "net profit / inventory": [self.net_profit_inventory],
-                "(current assets - inventory) / short-term liabilities": [
-                    self.current_assets_inventory_short_term_liabilities
-                ],
-                "(inventory * 365) / cost of products sold": [
-                    self.inventory_365_cost_of_products_sold
-                ],
-                "EBITDA (profit on operating activities - depreciation) / total assets": [
-                    self.EBITDA_profit_on_operating_activities_depreciation_total_assets
-                ],
-                "EBITDA (profit on operating activities - depreciation) / sales": [
-                    self.EBITDA_profit_on_operating_activities_depreciation_sales
-                ],
-                "current assets / total liabilities": [
-                    self.current_assets_total_liabilities
-                ],
-                "short-term liabilities / total assets": [
-                    self.short_term_liabilities_total_assets
-                ],
-                "(short-term liabilities * 365) / (cost of products sold)": [
-                    self.short_term_liabilities_365_cost_of_products_sold
-                ],
-                "equity / fixed assets": [self.equity_fixed_assets],
-                "constant capital / fixed assets": [self.constant_capital_fixed_assets],
-                "working capital": [self.working_capital],
-                "(sales - cost of products sold) / sales": [
-                    self.sales_cost_of_products_sold_sales
-                ],
-                "(current assets - inventory - short-term liabilities) / (sales - gross profit - depreciation)": [
-                    self.current_assets_inventory_short_term_liabilities_sales_gross_profit_depreciation
-                ],
-                "total costs /total sales": [self.total_costs_total_sales],
-                "long-term liabilities / equity": [self.long_term_liabilities_equity],
-                "sales / inventory": [self.sales_inventory],
-                "sales / receivables": [self.sales_receivables],
-                "(short-term liabilities *365) / sales": [
-                    self.short_term_liabilities_365_sales
-                ],
-                "sales / short-term liabilities": [self.sales_short_term_liabilities],
-                "sales / fixed assets": [self.sales_fixed_assets],
+                "Time": [self.Time],
+                "V1": [self.V1],
+                "V2": [self.V2],
+                "V3": [self.V3],
+                "V4": [self.V4],
+                "V5": [self.V5],
+                "V6": [self.V6],
+                "V7": [self.V7],
+                "V8": [self.V8],
+                "V9": [self.V9],
+                "V10": [self.V10],
+                "V11": [self.V11],
+                "V12": [self.V12],
+                "V13": [self.V13],
+                "V14": [self.V14],
+                "V15": [self.V15],
+                "V16": [self.V16],
+                "V17": [self.V17],
+                "V18": [self.V18],
+                "V19": [self.V19],
+                "V20": [self.V20],
+                "V21": [self.V21],
+                "V22": [self.V22],
+                "V23": [self.V23],
+                "V24": [self.V24],
+                "V25": [self.V25],
+                "V26": [self.V26],
+                "V27": [self.V27],
+                "V28": [self.V28],
+                "Amount": [self.Amount]
             }
-
             return pd.DataFrame(custom_data_input)
-
         except Exception as e:
-            raise CustomException(e, sys) from e
+            raise CustomException(e, None)
+    
+if __name__ == "__main__":
+    # Example: Provide dummy values for all required features
+    data = CustomData(
+        Time=0.0,
+        V1=0.0, V2=0.0, V3=0.0, V4=0.0, V5=0.0, V6=0.0, V7=0.0, V8=0.0, V9=0.0, V10=0.0,
+        V11=0.0, V12=0.0, V13=0.0, V14=0.0, V15=0.0, V16=0.0, V17=0.0, V18=0.0, V19=0.0, V20=0.0,
+        V21=0.0, V22=0.0, V23=0.0, V24=0.0, V25=0.0, V26=0.0, V27=0.0, V28=0.0,
+        Amount=0.0
+    )
+    pred_df = data.get_data_as_dataframe()
+    print("Input DataFrame:")
+    print(pred_df)
+    predict_pipeline = PredictPipeline()
+    pred_result = predict_pipeline.predict(pred_df)
+    print("Prediction result:", pred_result)
+
