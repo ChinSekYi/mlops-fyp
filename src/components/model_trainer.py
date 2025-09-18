a7297560a4a94be1edcdef2275042b193a14ea40
Chin Sek Yi <sekyichin@gmail.com>
2025-09-18 21:37:15 +0800

    update mlflow workflow

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,61 +16,52 @@ load_dotenv()
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join("models", "model.pkl")
     metrics_file_path = os.path.join("metrics", "metrics.json")
-    mlflow_data_path = "mlflow_data"  # root folder for all MLflow data
-    mlruns_path = os.path.join(mlflow_data_path, "mlruns")
-    mlartifacts_path = os.path.join(mlflow_data_path, "mlartifacts")
-    mlflow_db_path = os.path.join(mlflow_data_path, "mlflow.db")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-        os.makedirs(self.model_trainer_config.mlruns_path, exist_ok=True)
-        os.makedirs(self.model_trainer_config.mlartifacts_path, exist_ok=True)
         os.makedirs(os.path.dirname(self.model_trainer_config.trained_model_file_path), exist_ok=True)
         os.makedirs(os.path.dirname(self.model_trainer_config.metrics_file_path), exist_ok=True)
 
-        # Set MLflow tracking URI to local MLflow server
-        mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
-        mlflow.set_experiment(os.getenv("MLFLOW_EXPERIMENT"))
-
-    def initiate_model_trainer(self, train_arr_path, test_arr_path, artifact_path):
+    def initiate_model_trainer(self, processed_train_data_path, processed_test_data_path, artifact_path, registered_model_name):
         try:
-            train_array = np.load(train_arr_path)
-            test_array = np.load(test_arr_path)
+            #train_array = np.load(train_arr_path)
+            #test_array = np.load(test_arr_path)
+            train_df = pd.read_csv(processed_train_data_path)
+            test_df = pd.read_csv(processed_test_data_path)
 
             logging.info("Splitting training and test input data")
-            x_train, y_train = train_array[:, :-1], train_array[:, -1]
-            x_test, y_test = test_array[:, :-1], test_array[:, -1]
+            X_train = train_df.drop(columns=["Class"])
+            y_train = train_df["Class"]
+            X_test = test_df.drop(columns=["Class"])
+            y_test = test_df["Class"]
 
-                
             # Log parameters
             mlflow.log_param("model_type", "LogisticRegression")
             mlflow.log_param("max_iter", 1000)
 
             # Train model
             model = LogisticRegression(max_iter=1000)
-            model.fit(x_train, y_train)
+            model.fit(X_train, y_train)
             
             # Save model locally and log to MLflow
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=model,
-            )
-
-            # Create an example input from training features
-            input_example = pd.DataFrame(x_train[:5], columns=[f"feature_{i}" for i in range(x_train.shape[1])])
+            ) #TODO: can remove bc model is logged to mlflow
 
             model_info = mlflow.sklearn.log_model(
                 sk_model=model,
-                input_example=input_example,
-                name=artifact_path
+                input_example=X_train.iloc[[0]],
+                name=artifact_path,
+                registered_model_name=registered_model_name  # Optional. Uncomment if you want to register this model
             )
             mlflow.log_artifact("models/model.pkl")
 
             # Make predictions
-            y_train_pred = model.predict(x_train)
-            y_test_pred = model.predict(x_test)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
 
             # Evaluate
             training_metrics = evaluate_model(y_train, y_train_pred)
@@ -83,10 +74,8 @@ class ModelTrainer:
             for key, value in testing_metrics.items():
                 mlflow.log_metric(f"test_{key}", value, model_id=model_info.model_id)
             
-            #mlflow.log_metric(model_id=model_info.model_id)
-
             # Save metrics.json for DVC tracking
-            os.makedirs("metrics", exist_ok=True)
+            os.makedirs("metrics", exist_ok=True)  #TODO: can remove bc model is logged to mlflow
             metrics_data = {
                 "training_data_metrics": training_metrics,
                 "testing_data_metrics": testing_metrics
@@ -101,9 +90,10 @@ class ModelTrainer:
 
 if __name__ == "__main__":
     modeltrainer = ModelTrainer()
-    train_arr_path = "data/processed/train_transformed.npy"
-    test_arr_path = "data/processed/test_transformed.npy"
-    training_metrics, testing_metrics = modeltrainer.initiate_model_trainer(train_arr_path, test_arr_path, artifact_path= "LogisticRegressionModelv1")
+    processed_train_data_path = os.path.join("data","processed", "processed_train.csv")
+    processed_test_data_path = os.path.join("data", "processed", "processed_test.csv")
+    
+    training_metrics, testing_metrics = modeltrainer.initiate_model_trainer(processed_train_data_path, processed_test_data_path, artifact_path= "LogisticRegressionModelv1")
 
     print(f"training_data results: {training_metrics}")
     print(f"testing_data results: {testing_metrics}")
--- src/pipeline/run_pipeline.py
+++ src/pipeline/run_pipeline.py
@@ -1,29 +1,51 @@
 import mlflow
+from mlflow import MlflowClient
+
 import os
 from src.components.model_trainer import ModelTrainer
 from src.components.data_transformation import DataTransformation
 from src.components.data_ingestion import DataIngestion
+from dotenv import load_dotenv
+load_dotenv()
 
 # Set MLflow tracking URI to local MLflow server
+RUN_NAME = "xgboost_v8"
+DATASET_NAME = "creditcard.csv"
+ARTIFACT_PATH = "xgboost_model"
+ALGORITHM_TYPE = "xgboost"
+REGISTERED_MODEL_NAME = "fraud-detection-model"
+
+# set MLFLOW_EXPERIMENT and MLFLOW_EXPERIMENT in .env
 mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
 mlflow.set_experiment(os.getenv("MLFLOW_EXPERIMENT"))
 
 # Start MLflow run
-with mlflow.start_run(run_name="logistic_regression_test_13"):
-    mlflow.set_tag("team", "pacman")
-    dataset_name = "creditcard.csv"
+with mlflow.start_run(run_name=RUN_NAME):
+    mlflow.set_tag("algorithm", ALGORITHM_TYPE)
     
     # ingestion
     obj = DataIngestion()
-    train_data_path, test_data_path = obj.initiate_data_ingestion(dataset_name)
+    train_data_path, test_data_path = obj.initiate_data_ingestion(DATASET_NAME)
 
     # transformation
     data_transformation = DataTransformation()
-    train_arr_path, test_arr_path, _ = data_transformation.initiate_data_transformation(
+    train_arr_path, test_arr_path, _  = data_transformation.initiate_data_transformation(
         train_data_path, test_data_path
     )
 
     # training
     modeltrainer = ModelTrainer()
-    artifact_path = "logistic_regression_model"
-    training_metrics, testing_metrics = modeltrainer.initiate_model_trainer(train_arr_path, test_arr_path, artifact_path)
+
+    # enter registered_model_name in model_trainer, if required
+    training_metrics, testing_metrics = modeltrainer.initiate_model_trainer(train_arr_path, test_arr_path, ARTIFACT_PATH, REGISTERED_MODEL_NAME)
+
+    # set model registry tags
+    # comment if this run is not saved in registry
+
+client = MlflowClient()
+client.set_model_version_tag(
+    REGISTERED_MODEL_NAME,
+    '21',
+    "algorithm",
+    ALGORITHM_TYPE
+)
