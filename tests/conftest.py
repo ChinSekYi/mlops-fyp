3ff7689d171987ae960d637b2706952bf8b5b7f0
Chin Sek Yi <sekyichin@gmail.com>
2025-09-26 04:32:33 +0800

    fix conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,8 @@ from dotenv import load_dotenv
 from api.main import load_model
 
 load_dotenv()
+MODEL_NAME=os.getenv("MODEL_NAME")
+MODEL_ALIAS=os.getenv("MODEL_ALIAS")
 MODEL_SERVER_IP = os.getenv("MODEL_SERVER_IP")
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
@@ -23,7 +25,8 @@ mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 @pytest.fixture(scope="module")
 def model():
     """Load the ML model once for all tests."""
-    mdl = load_model()
+    model_uri = f"models:/{MODEL_NAME}@{MODEL_ALIAS}"
+    mdl =  mlflow.sklearn.load_model(model_uri)
     assert mdl is not None
     return mdl
 
