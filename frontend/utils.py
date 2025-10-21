d668725ed9d0f68dc43804611bd67dfbc63c0f2e
Chin Sek Yi <sekyichin@gmail.com>
2025-10-21 15:32:46 +0800

    fix frontend bug

--- frontend/utils.py
+++ frontend/utils.py
@@ -9,7 +9,6 @@ def load_environment(env_file: str = None):
     Default is .env at project root's /env folder.
     """
     project_root = os.path.dirname(os.path.dirname(__file__))
-    env_dir = os.path.join(project_root, "env")
     if env_file is None:
         env_file = "env/.env"
     env_path = (
@@ -17,7 +16,5 @@ def load_environment(env_file: str = None):
         if not os.path.isabs(env_file)
         else env_file
     )
-    if not os.path.exists(env_path):
-        env_path = os.path.join(env_dir, ".env")
     load_dotenv(env_path)
     print(f"[ENV] Loaded environment from {env_path}")
